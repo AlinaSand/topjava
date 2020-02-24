d0dbfc87ae2051c7ac48378794d8d93f64b2b631
alinasandybaeva <sand-alina2412@yandex.ru>
2020-02-24 22:23:39 +0300

    Fix MealTestData, MealServiceTest, UserServiceTest. Deleted spring-jdbc.xml

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -26,8 +26,7 @@ import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 @ContextConfiguration({
         "classpath:spring/spring-app.xml",
-        "classpath:spring/spring-db.xml",
-        "classpath:spring/spring-jdbc.xml"
+        "classpath:spring/spring-db.xml"
 })
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
